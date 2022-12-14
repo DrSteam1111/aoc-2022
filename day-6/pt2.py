b88e47274ca6310c4a434965a64d347e6789f61e
Edvin Karlsson <edvinkarlsson9@gmail.com>
2022-12-14 14:42:36 +0100

    Day 6

---
+++ day-6/pt2.py
@@ -0,0 +1,46 @@
+import sys;
+
+def main():
+    input_file = open('input.txt', 'r')
+    lines = input_file.read()
+
+    sys.setrecursionlimit(5000)
+    end = findSubroutine(lines)
+    print(end)
+    
+
+def findSubroutine(lines):
+    start = 0
+    end = 0
+    found = False
+
+    def find():
+        nonlocal start
+        nonlocal end
+        nonlocal found
+
+        local_tracker = 0
+        char_array = []
+
+        for char in lines[start:]:
+            local_tracker += 1
+            if char in char_array:
+                start += 1
+                break
+            
+            char_array.append(char)
+            
+            if local_tracker == 14:
+                end = start + 14
+                found = True
+                break
+        
+        if found == False:
+            find()
+
+    find()
+    return end
+
+
+if __name__ == '__main__':
+    main()
