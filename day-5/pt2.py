e7b6f58385f15bf6752d16eb44ca8a792f749575
Edvin Karlsson <edvinkarlsson9@gmail.com>
2022-12-14 14:42:06 +0100

    Day 5

---
+++ day-5/pt2.py
@@ -0,0 +1,67 @@
+import re
+from dataclasses import dataclass
+
+@dataclass
+class Instruction:
+    amount: int
+    from_index: int
+    to_index: int
+
+    def __init__(self, instruction):
+        regex = re.search('move (.*) from (.*) to (.*)', instruction)
+        self.amount = int(regex.group(1))
+        self.from_index = int(regex.group(2)) - 1
+        self.to_index = int(regex.group(3)) - 1
+        
+
+def main():
+    input_file = open('input.txt', 'r')
+    lines = input_file.readlines()
+
+    crates = parseCrates(lines)
+    instructions = createInstructions(lines)
+    
+    movedCrates = doInstructions(crates, instructions)
+    print(formatAnswer(movedCrates))
+
+def parseCrates(lines):
+    groups = [[] for i in range(9)]
+    for i in range(8):
+        strippedLine = lines[i].replace('\n', '')
+        groupedLine = re.sub('    ', ' ', strippedLine);
+        lineArray = re.split('\s', groupedLine);
+
+        for idx, char in enumerate(lineArray):
+            if char != '':
+                groups[idx].append(char);
+            
+    return groups
+
+def createInstructions(lines):
+    instructions = []
+    for line in lines[10:]:
+        instructions.append(Instruction(line))
+
+    return instructions
+
+def doInstructions(crates, instructions):
+    afterInst = crates
+    for inst in instructions:
+        to_move = [];
+        for _ in range(inst.amount):
+            to_move.append(afterInst[inst.from_index].pop(0))
+
+        afterInst[inst.to_index] = to_move + afterInst[inst.to_index]
+    return afterInst
+
+def formatAnswer(movedCrates):
+    stringAnswer = ''
+    for m_crates in movedCrates:
+        stringAnswer += m_crates[0]
+
+    stringAnswer = re.sub('\[|\]', '', stringAnswer)
+
+    return stringAnswer
+
+if __name__ == '__main__':
+    main()
